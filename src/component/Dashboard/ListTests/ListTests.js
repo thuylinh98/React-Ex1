423712ea7947d7070a8377c1f17db8eacfb4cb68
thuylinh98 <linhlinh4125@gmail.com>
2021-08-09 09:51:21 +0700

    update review

--- src/component/Dashboard/ListTests/ListTests.js
+++ src/component/Dashboard/ListTests/ListTests.js
@@ -13,17 +13,26 @@ class ListTests extends Component{
       sort: 'all',
       showTest : [],
       TestList: [],
-      soluong: 8
+      amount: 8, 
+      page: 1,
+      List: [{name: 'Test 1',time: '1',point:'1',difficult: '1',evaluate: 1,status: true},
+            {name: 'Test 2',time: '1',point:'1',difficult: '1',evaluate: 0,status: false},
+            {name: 'Test 3',time: '1',point:'1',difficult: '1',evaluate: 0,status: false},
+            {name: 'Test 4',time: '1',point:'1',difficult: '1',evaluate: 0,status: false},
+            {name: 'Test 5',time: '1',point:'1',difficult: '1',evaluate: 0,status: false},
+            {name: 'Test 6',time: '1',point:'1',difficult: '1',evaluate: 0,status: false},
+            {name: 'Test 7',time: '1',point:'1',difficult: '1',evaluate: 0,status: false},
+            {name: 'Test 8',time: '1',point:'1',difficult: '1',evaluate: 0,status: false},
+            {name: 'Test 9',time: '1',point:'1',difficult: '1',evaluate: 0,status: false},
+            {name: 'Test 10',time: '1',point:'1',difficult: '1',evaluate: 0,status: true}]
     }
-   
+    this.handleChangeFilter = this.handleChangeFilter.bind(this);
   }
   componentDidMount() {
-    this.setState({showTest: this.List.slice(0,this.state.soluong), TestList: this.List});
+    this.setState({showTest: this.state.List.slice(0,this.state.amount), TestList: this.state.List});
   }
-  onChange=(event)=>{
-    var target=event.target;
-    var name = target.name;
-    var value = target.value;
+  onChangeInput=(event)=>{
+    const { name, value} = event.target;
     this.setState({
       [name]: value
     })
@@ -33,75 +42,58 @@ class ListTests extends Component{
     this.search(this.state.search);
     console.log(this.state.showTest)
   }
-  taodayso = ()=>{
+  listPage = ()=>{
     var arr = [];
-    var number=this.state.TestList.length/this.state.soluong;
-    if (this.state.TestList.length%this.state.soluong >=0) number++;
+    var number=this.state.TestList.length/this.state.amount;
+    if (this.state.TestList.length%this.state.amount >=0) number++;
     for (let i=1; i<number;i++){
       arr.push(i);
     }
-    console.log(arr);
+    console.log(this.state.amount);
     return arr.map((element) => {
       return(
         <RowNB key={element}>
-      
-        <ButtonNB  onClick={() => this.chuyenTrang(element)}>{element}</ButtonNB>
+        <ButtonNB  onClick={() => this.nextPage(element)}>{element}</ButtonNB>
         </RowNB>
       )
     });
   };
-  chuyenTrang = (number) =>{
-    var end = this.state.soluong*number;
-    this.setState({showTest: this.state.TestList.slice(end-8,end)})
-
+  nextPage = (number) =>{
+    var end = this.state.amount*number;
+    this.setState({showTest: this.state.TestList.slice(end-8,end), page: number})
+    console.log("number",this.page)
   };
   search = (element) =>{
     var arr = [];
-    for (let x of this.List){
-      if (x.name==( element)) 
+    for (let x of this.state.List){
+      if (x.name===( element)) 
         arr.push(x);
     }
-    this.setState({TestList: arr, showTest: arr.slice(0,this.state.soluong)})
-    this.taodayso();
+    this.setState({TestList: arr, showTest: arr.slice(0,this.state.amount)})
+    this.listPage();
   };
-  handleChange=(event)=>{
+  handleChangeFilter=(event)=>{
     var arr=[];
-    var target=event.target;
-    var name = target.name;
-    var value = target.value;
-    this.setState({
-      [name]: value
-    })
-    console.log(value)
-    if (value === "All") {arr=this.List}
-    else if (value ==="Test Complete"){
-      for (let x of this.List){
+    let sort = event.target.value;
+    this.setState({sort: sort});
+    console.log(this.state.sort)
+    if (sort === "All") {arr=this.state.List}
+    else if (sort ==="Test Complete"){
+      for (let x of this.state.List){
         if (x.status) 
           arr.push(x);
     }
     }else{
-      for (let x of this.List){
+      for (let x of this.state.List){
         if (!x.status) 
           arr.push(x);
     }
     }
-    this.setState({TestList: arr, showTest: arr.slice(0,this.state.soluong)})
-    this.taodayso();
+    this.setState({TestList: arr, showTest: arr.slice(0,this.state.amount)})
+    this.listPage();
     console.log("list",this.state.TestList)
   }
-  List=[
-    {name: 'Test 1',time: '1',point:'1',difficult: '1',evaluate: 1,status: true},
-    {name: 'Test 2',time: '1',point:'1',difficult: '1',evaluate: 0,status: false},
-    {name: 'Test 3',time: '1',point:'1',difficult: '1',evaluate: 0,status: false},
-    {name: 'Test 4',time: '1',point:'1',difficult: '1',evaluate: 0,status: false},
-    {name: 'Test 5',time: '1',point:'1',difficult: '1',evaluate: 0,status: false},
-    {name: 'Test 6',time: '1',point:'1',difficult: '1',evaluate: 0,status: false},
-    {name: 'Test 7',time: '1',point:'1',difficult: '1',evaluate: 0,status: false},
-    {name: 'Test 8',time: '1',point:'1',difficult: '1',evaluate: 0,status: false},
-    {name: 'Test 9',time: '1',point:'1',difficult: '1',evaluate: 0,status: false},
-    {name: 'Test 10',time: '1',point:'1',difficult: '1',evaluate: 0,status: true}
-
-  ]
+  
 render(){
   return(
     <Wrapper>
@@ -111,7 +103,7 @@ render(){
                 name="search"
                 required
                 value={this.state.search}
-                onChange={this.onChange}
+                onChange={this.onChangeInput}
                 placeholder="Test name"></Input>
           <Button onClick={() => this.goTo()}>Go to</Button>
         </Column>
@@ -119,7 +111,7 @@ render(){
           <select name="sort"
                 required
                 value={this.state.sort}
-                onChange={this.handleChange}>
+                onChange={this.handleChangeFilter}>
             <option>All</option>
             <option>Test Complete</option>
             <option>Test Not Complete</option>
@@ -132,7 +124,9 @@ render(){
           <Test TestData={TestData} />) :  <Text>Không tìm thấy kết quả phù hợp</Text>
         }
       </RowTest>
-      <RowNB>{this.taodayso()}</RowNB>
+      <RowNB>
+        {this.listPage()}
+      </RowNB>
     </Wrapper>
   );
 }
